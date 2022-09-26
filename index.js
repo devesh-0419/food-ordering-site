0d3f494b47c60a69dcbe4b855b53f507d29ffd66
Devesh Swarnkar <deveshswarnkar007@gmail.com>
2022-09-26 15:13:44 +0530

    Connected to database

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors =require('cors')
+const mongoose = require('mongoose');
+const cookieParser=require('cookie-parser');
+const app = express();
+require('dotenv').config();
+
+
+// app.use(cors({
+//      origin: ["http://localhost:3000", "https://dine-it.netlify.app","http://192.168.43.230:3000/"]
+//     ,
+//     credentials:true
+// }));
+
+app.use(cookieParser())
+ 
+app.use(express.json());
+
+
+
+ 
+mongoose.connect(process.env.DB_URI).then(
+    app.listen(process.env.PORT|| 8443 ,(err)=>{
+        if(err) console.error(err)
+        else
+           console.log(`server is up and running on port ${process.env.PORT} or 8443 and db connected`);
+           
+    })  
+    
+).catch(err=>console.error(err));
+                    
+   
